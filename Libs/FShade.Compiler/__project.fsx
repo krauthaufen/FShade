c93d5959e39000831b354647b7dba6635e0a1544
Harald Steinlechner <steinlechner@vrvis.at>
2014-08-28 17:41:16 +0000

    topological sorting for dependencies which seems to be novel in .net since no tool or script can do it properly :o

---
+++ Libs/FShade.Compiler/__project.fsx
@@ -0,0 +1,7 @@
+#r "System"
+#r "System.Core"
+#r "System.Drawing"
+#r "System.Numerics"
+#r @".\..\..\BinPrebuilt\Release\Aardvark.Base.TypeProviders.dll"
+#r @".\..\..\Bin\Debug\Aardvark.Base.FSharp.dll"
+#r @".\..\..\Bin\Debug\Aardvark.Base.dll"
