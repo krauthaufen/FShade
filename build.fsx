51d0a4976efc34f812819b69f78afc102b01f967
krauthaufen <krauthaufen@awx.at>
2015-01-10 13:45:10 +0100

    some folder reorganization;

--- build.fsx
+++ build.fsx
@@ -4,6 +4,10 @@
 open Fake
 
 
+let core = ["Libs/FShade.Compiler/FShade.Compiler.fsproj"; "Libs/FShade/FShade.fsproj"; "Libs/FShade.Debug/FShade.Debug.fsproj"];
+let demo = ["Apps/FShade.DemoRenderer/FShade.DemoRenderer.fsproj"; "Apps/FShade.Demo/FShade.Demo.fsproj"];
+let apps = ["Apps/FSCC/FSCC.fsproj"; "Apps/FSCC.Service/FSCC.Service.fsproj"]
+
 Target "Restore" (fun () ->
     RestorePackages()
 )
@@ -12,15 +16,27 @@ Target "Clean" (fun () ->
     CleanDir "Bin"
 )
 
+Target "Core" (fun () ->
+    MSBuildRelease "Bin/Release" "Build" core |> ignore
+)
 
-Target "Compile" (fun () ->
-    MSBuildRelease "Bin/Release" "Build" ["FShade.sln"] |> ignore
+Target "Demo" (fun () ->
+    MSBuildRelease "Bin/Release" "Build" demo |> ignore
+)
+
+Target "Apps" (fun () ->
+    MSBuildRelease "Bin/Release" "Build" apps |> ignore
 )
 
 
+Target "Compile" (fun () -> ())
 Target "Default" (fun () -> ())
 Target "Rebuild" (fun () -> ())
 
+"Core" ==>
+    "Demo" ==>
+    "Apps" ==>
+    "Compile"
 
 "Restore" ==> 
     "Compile" ==>
