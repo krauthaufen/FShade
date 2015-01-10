f2036536cf10242598847e109b795661ddc97d21
krauthaufen <krauthaufen@awx.at>
2015-01-10 14:35:36 +0100

    added NuGet-Package-Creation to build script;

--- build.fsx
+++ build.fsx
@@ -2,7 +2,8 @@
 #r @"FakeLib.dll"
 
 open Fake
-
+open System
+open System.IO
 
 let core = ["src/Libs/FShade.Compiler/FShade.Compiler.fsproj"; "src/Libs/FShade/FShade.fsproj"; "src/Libs/FShade.Debug/FShade.Debug.fsproj"];
 let demo = ["src/Apps/FShade.DemoRenderer/FShade.DemoRenderer.fsproj"; "src/Apps/FShade.Demo/FShade.Demo.fsproj"];
@@ -29,7 +30,6 @@ Target "Apps" (fun () ->
 )
 
 
-
 Target "Compile" (fun () -> ())
 Target "Default" (fun () -> ())
 Target "Rebuild" (fun () -> ())
@@ -53,6 +53,30 @@ Target "Build" (fun () -> ())
 "Clean" ==> "Rebuild"
 "Compile" ==> "Rebuild"
 "Compile" ==> "Build"
+
+
+Target "CreatePackage" (fun () ->
+    NuGetPack (fun p -> { p with OutputPath = "Bin" }) "NuGet/FShade.nuspec"
+)
+
+Target "Deploy" (fun () ->
+
+    let accessKeyPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "nuget.key")
+    let accessKey =
+        if File.Exists accessKeyPath then Some (File.ReadAllText accessKeyPath)
+        else None
+    match accessKey with
+        | Some accessKey ->
+            try
+                NuGet (fun p -> let r = { p with Title = "FShade"; Project = "FShade"; OutputPath = "Bin"; AccessKey = accessKey; Publish = true } in printfn "%A" r; r) "NuGet/FShade.nuspec"
+            with e ->
+                ()
+        | None ->
+            ()
+)
+
+"Core" ==> "CreatePackage"
+"CreatePackage" ==> "Deploy"
 
 // start build
 RunTargetOrDefault "Default"
