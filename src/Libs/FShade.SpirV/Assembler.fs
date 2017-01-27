9d2a993d9922fa36c127e1b2241b429cf1ec1145
krauthaufen <krauthaufen@awx.at>
2017-01-27 15:38:46 +0100

    [SpirV] started assembler rewrite

--- src/Libs/FShade.SpirV/Assembler.fs
+++ src/Libs/FShade.SpirV/Assembler.fs
@@ -860,3 +860,497 @@ module Assembler =
                     
 
         }
+
+    let assembleEntryParameter (clazz : StorageClass) (p : CEntryParameter) =
+        asm {
+            let! t = assemblePtrType clazz p.cParamType
+            let! id = newId
+            yield OpVariable(t, id, clazz, None)
+
+
+
+        }
+
+    let assembleEntry (e : CEntryDef) =
+        asm {
+            
+            ()
+        }
+
+
+module Assembler2 =
+
+    type SpirVState =
+        {
+            currentId           : uint32
+            valueIds            : HashMap<obj, uint32>
+            uniformIds          : Map<string, uint32 * list<uint32>>
+            fieldIds            : HashMap<CType, HashMap<string, int>>
+            reversedInstuctions : list<Instruction>
+            currentBinding      : uint32
+            currentSet          : uint32
+        }
+
+    type SpirV<'a> = State<SpirVState, 'a>
+
+    type SpirVBuilder() =
+        inherit StateBuilder()
+
+        member x.Yield(i : Instruction) =
+            State.modify (fun s ->
+                { s with reversedInstuctions = i :: s.reversedInstuctions }
+            )
+
+        member x.Run(m : SpirV<'a>) : SpirV<'a> =
+            m
+
+    let spirv = SpirVBuilder()
+
+    module SpirV =
+        let tryGetId (a : 'a) : SpirV<Option<uint32>> =
+            State.get |> State.map (fun s -> HashMap.tryFind (a :> obj) s.valueIds)
+            
+        let setId (a : 'a) (id : uint32) : SpirV<unit> =
+            State.modify (fun s -> { s with valueIds = HashMap.add (a :> obj) id  s.valueIds })
+
+            
+        let setUniformId (name : string) (var : uint32) (fields : list<uint32>) : SpirV<unit> =
+            State.modify (fun s -> { s with uniformIds = Map.add name (var, fields) s.uniformIds })
+
+
+        type CachedSpirVBuilder(key : obj) =
+            inherit StateBuilder()
+
+            member x.Yield(i : Instruction) =
+                State.modify (fun s ->
+                    { s with reversedInstuctions = i :: s.reversedInstuctions }
+                )
+
+            member x.Run(m : SpirV<uint32>) : SpirV<uint32> =
+                state {
+                    let! v = tryGetId key 
+                    match v with
+                        | Some id -> 
+                            return id
+                        | None ->
+                            let! id = m
+                            do! setId key id
+                            return id
+                }
+
+        let cached (v : 'a) =
+            CachedSpirVBuilder(v :> obj)
+
+        let id = 
+            State.custom (fun s ->
+                let id = s.currentId
+                { s with currentId = id + 1u }, id
+            )
+
+        let setFieldId (t : CType) (name : string) (id : int) =
+            State.modify (fun s ->
+                match HashMap.tryFind t s.fieldIds with
+                    | Some ids ->
+                        { s with fieldIds = HashMap.add t (HashMap.add name id ids) s.fieldIds }
+                    | None ->
+                        { s with fieldIds = HashMap.add t (HashMap.ofList [name,id]) s.fieldIds }
+            )
+
+        let tryGetFieldId (t : CType) (name : string) =
+            State.get |> State.map (fun s ->
+                match HashMap.tryFind t s.fieldIds with
+                    | Some ids ->
+                        HashMap.tryFind name ids
+                    | None ->
+                        None
+            )
+
+        let newBinding : SpirV<uint32> =
+            State.custom (fun s ->
+                let c = s.currentBinding
+                { s with currentBinding = c + 1u }, c
+            )
+
+        let newSet : SpirV<uint32> =
+            State.custom (fun s ->
+                let c = s.currentSet
+                { s with currentSet = c + 1u; currentBinding = 0u }, c
+            )
+
+    let rec assembleType (t : CType) =
+        SpirV.cached t {
+            match t with
+                | CType.CVoid -> 
+                    let! id = SpirV.id
+                    yield OpTypeVoid id
+                    return id
+
+                | CType.CBool ->
+                    let! id = SpirV.id
+                    yield OpTypeBool id
+                    return id
+                
+                | CType.CInt(s,w) ->
+                    let! id = SpirV.id
+                    yield OpTypeInt(id, uint32 w, (if s then 1u else 0u))
+                    return id
+
+                | CType.CFloat(32 | 64) ->
+                    let! id = SpirV.id
+                    yield OpTypeFloat(id, 32u)
+                    return id
+
+                | CType.CFloat(w) ->
+                    let! id = SpirV.id
+                    yield OpTypeFloat(id, uint32 w)
+                    return id
+
+                | CType.CVector(e,d) ->
+                    let! e = assembleType e
+                    let! id = SpirV.id
+                    yield OpTypeVector(id, e, uint32 d)
+                    return id
+
+                | CType.CMatrix(e,r,c) ->
+                    let! ev = assembleType (CVector(e, r))
+                    let! id = SpirV.id
+                    yield OpTypeMatrix(id, ev, uint32 c)
+                    return id
+                    
+                | CType.CArray(e,l) ->
+                    let! e = assembleType e
+                    let! id = SpirV.id
+                    yield OpTypeArray(id, e, uint32 l)
+                    return id
+                    
+                | CType.CPointer(_,e) ->
+                    let! e = assembleType e
+                    let! id = SpirV.id
+                    yield OpTypeRuntimeArray(id, e)
+                    return id
+                    
+                | CType.CStruct(name, fields, _) ->
+                    let fields = List.toArray fields
+                    let! fieldTypes = fields |> Array.mapS (fst >> assembleType)
+
+                    let! id = SpirV.id
+                    yield OpTypeStruct(id, fieldTypes)
+
+                    yield OpName(id, name)
+                    for i in 0 .. fields.Length - 1 do
+                        let (_,n) = fields.[i]
+                        yield OpMemberName(id, uint32 i, n)
+                        do! SpirV.setFieldId t n i
+
+                    return id
+
+                | CType.CIntrinsic it ->
+                    return! assembleIntrinsicType it
+                    
+        }
+
+    and assemblePtrType (clazz : StorageClass) (t : CType) =
+        SpirV.cached (clazz, t) {
+            let! t = assembleType t
+            let! id = SpirV.id
+            yield OpTypePointer(id, clazz, t)
+            return id
+        }
+
+    and assembleIntrinsicType (t : CIntrinsicType) : SpirV<uint32> =
+        spirv {
+            return failwith "not implemented"
+        }
+
+    let rec assembleExpr (s : CExpr) : SpirV<uint32> =
+        spirv {
+            return failwith "not implemented"
+        }
+
+    let rec assembleRExpr (s : CRExpr) : SpirV<uint32> =
+        spirv {
+            return failwith "not implemented"
+        }
+
+    let rec assembleLExpr (s : CRExpr) : SpirV<uint32> =
+        spirv {
+            return failwith "not implemented"
+        }
+   
+    let rec assembleStatement (s : CStatement) : SpirV<unit> =
+        spirv {
+            return failwith "not implemented"
+        }
+
+    let tryGetBuiltIn (kind : ParameterKind) (stage : ShaderStageDescription) (name : string) : Option<BuiltIn> =
+        failwith "not implemented"
+
+
+
+    let assembleFunctionType (args : array<uint32>) (ret : uint32) =
+        SpirV.cached ("fun", args, ret) {  
+            let! id = SpirV.id
+            yield OpTypeFunction(id, ret, args)
+            return id
+        }
+
+    let rec assembleLiteral (t : CType) (v : CLiteral) =
+        SpirV.cached (t, v) {
+            let! tid = assembleType t
+            let! id = SpirV.id
+            match t, v with
+                | CInt(_, 8), CIntegral v ->
+                    yield OpConstant(tid, id, [| uint32 (int8 v) |])
+                | CInt(_, 16), CIntegral v ->
+                    yield OpConstant(tid, id, [| uint32 (int16 v) |])
+                | CInt(_, 32), CIntegral v ->
+                    yield OpConstant(tid, id, [| uint32 (int32 v) |])
+                | CInt(_, 64), CIntegral v ->
+                    yield OpConstant(tid, id, [| uint32 (v >>> 32); uint32 v |])
+
+                | CFloat(32 | 64), CFractional v ->
+                    let value = v |> BitConverter.GetBytes
+                    yield OpConstant(tid, id, [| BitConverter.ToUInt32(value, 0) |])
+                    
+                | CFloat(16), CFractional v ->
+                    let mutable value = float16()
+                    value.Float32 <- float32 v
+                    yield OpConstant(tid, id, [| uint32 value.UInt16 |])
+
+                | _, CBool v ->
+                    if v then yield OpConstantTrue(tid, id)
+                    else yield OpConstantFalse(tid, id)
+
+                | _, Null ->
+                    yield OpConstantNull(tid, id)
+
+                | _ ->
+                    failwithf "[FShade] unsupported literal value %A : %A" v t
+
+            return id
+        }
+
+    let rec assembleConstantExpr (e : CExpr) =
+        spirv {
+            match e with
+                | CValue(t,v) ->
+                    return! assembleLiteral t v
+
+                | CNewVector(t, d, comp) ->
+                    let! tid = assembleType t
+                    let! comp = comp |> List.mapS assembleConstantExpr
+                    let! id = SpirV.id
+                    yield OpConstantComposite(tid, id, List.toArray comp)
+                    return id
+
+
+                | _ ->
+                    return failwithf "[FShade] not a constant expression %A" e
+        }
+
+    let assembleConstantRExpr (e : CRExpr) =
+        spirv {
+            match e with
+                | CRExpr e -> 
+                    return! assembleConstantExpr e
+
+                | CRArray(t,args) ->
+                    let! t = assembleType t
+                    let! args = args |> List.mapS assembleConstantExpr
+                    let! id = SpirV.id
+                    yield OpConstantComposite(t, id, List.toArray args)
+                    return id
+        }
+
+
+    let assembleEntryDef (cond : Option<string>) (e : CEntryDef) =
+        spirv {
+            let args = e.cArguments |> List.toArray
+
+            let! ret = assembleType e.cReturnType
+            let! argTypes = args |> Array.mapS (fun a -> a.cParamType |> assembleType)
+            let! fType = assembleFunctionType argTypes ret
+
+            let mutable inputLocation = 0
+            let mutable outputLocation = 0
+
+            let stages = e.cDecorations |> List.pick (function (EntryDecoration.Stages s) -> Some s | _ -> None)
+            let model =
+                match stages.self with
+                    | ShaderStage.Vertex -> ExecutionModel.Vertex
+                    | ShaderStage.TessControl -> ExecutionModel.TessellationControl
+                    | ShaderStage.TessEval -> ExecutionModel.TessellationEvaluation
+                    | ShaderStage.Geometry -> ExecutionModel.Geometry
+                    | ShaderStage.Fragment -> ExecutionModel.Fragment
+                    | _ -> ExecutionModel.GLCompute
+
+            let iface = System.Collections.Generic.List<uint32>()
+
+            for i in e.cInputs do
+                let! t = assembleType i.cParamType
+                let! id = SpirV.id
+                yield OpVariable(t, id, StorageClass.Input, None)
+                yield OpName(id, i.cParamName)
+                match tryGetBuiltIn ParameterKind.Input stages i.cParamName with
+                    | Some b ->
+                        yield OpDecorate(id, Decoration.BuiltIn, [| uint32 b |])
+                    | None ->   
+                        yield OpDecorate(id, Decoration.Location, [| uint32 inputLocation |])
+                        inc &inputLocation
+
+                do! SpirV.setId (ParameterKind.Input, i.cParamName) id
+                iface.Add id
+
+            for i in e.cOutputs do
+                let! t = assembleType i.cParamType
+                let! id = SpirV.id
+                yield OpVariable(t, id, StorageClass.Output, None)
+                yield OpName(id, i.cParamName)
+                match tryGetBuiltIn ParameterKind.Output stages i.cParamName with
+                    | Some b ->
+                        yield OpDecorate(id, Decoration.BuiltIn, [| uint32 b |])
+                    | None ->   
+                        yield OpDecorate(id, Decoration.Location, [| uint32 outputLocation |])
+                        inc &outputLocation
+
+                do! SpirV.setId (ParameterKind.Output, i.cParamName) id
+                iface.Add id
+
+            let entryName =
+                match cond with
+                    | Some c -> c
+                    | None -> e.cEntryName
+
+            let! eid = SpirV.id
+
+            yield OpEntryPoint(model, eid, entryName, iface.ToArray())
+            yield OpFunction(ret, eid, FunctionControl.None, fType)
+            yield OpName(eid, entryName)
+
+            for ai in 0 .. argTypes.Length - 1 do
+                let name = args.[ai].cParamName
+                let tid = argTypes.[ai]
+                let! id = SpirV.id
+                yield OpFunctionParameter(tid, id)
+                yield OpName(id, name)
+                do! SpirV.setId (ParameterKind.Argument, name) id
+
+
+            do! assembleStatement e.cBody
+
+            yield OpFunctionEnd
+
+        }
+
+    let rec assembleValueDef (cond : Option<string>) (d : CValueDef) : SpirV<unit> =
+        spirv {
+            match d with
+                | CValueDef.CConstant(t, name, value) ->
+                    let! value = assembleConstantRExpr value
+                    yield OpName(value, name)
+                    do! SpirV.setId name value
+                    
+                | CValueDef.CUniformDef uniforms ->
+                    let buffers =
+                        uniforms 
+                            |> List.groupBy (fun u -> u.cUniformBuffer)
+
+                    let! set = SpirV.newSet
+                    for (name, fields) in buffers do
+                        match name with
+                            | Some name ->
+                                let! binding = SpirV.newBinding
+
+                                let t = CStruct(name, fields |> List.map (fun f -> f.cUniformType, f.cUniformName), None)
+                                let! tid = assemblePtrType StorageClass.Uniform t
+
+                                yield OpDecorate(tid, Decoration.GLSLPacked, [||])
+                                yield OpDecorate(tid, Decoration.BufferBlock, [||])
+
+                                let! id = SpirV.id
+                                yield OpVariable(tid, id, StorageClass.Uniform, None)
+                                yield OpDecorate(id, Decoration.DescriptorSet, [| set |])
+                                yield OpDecorate(id, Decoration.Binding, [| binding |])
+
+                                let fields = List.toArray fields
+                                for i in 0 .. fields.Length do
+                                    let u = fields.[i]
+                                    do! SpirV.setUniformId u.cUniformName id [uint32 i]
+
+
+                                ()
+
+                            | None ->
+                                for f in fields do
+                                    let! tid = assemblePtrType StorageClass.Uniform f.cUniformType
+                                    let! binding = SpirV.newBinding
+                                    let! id = SpirV.id
+                                    yield OpVariable(tid, id, StorageClass.Uniform, None)
+                                    yield OpDecorate(id, Decoration.DescriptorSet, [| set |])
+                                    yield OpDecorate(id, Decoration.Binding, [| binding |])
+                                    do! SpirV.setUniformId f.cUniformName id []
+        
+                | CValueDef.CConditionalDef(cond, inner) ->
+                    for v in inner do
+                        do! assembleValueDef (Some cond) v
+
+                | CValueDef.CFunctionDef(signature, body) ->
+                    let! argTypes = signature.parameters |> Array.mapS (fun p -> assembleType p.ctype)
+                    let! ret = assembleType signature.returnType
+                    let! tFun = assembleFunctionType argTypes ret
+
+                    let! id = SpirV.id
+                    yield OpFunction(ret, id, FunctionControl.None, tFun)
+                    for i in 0 .. argTypes.Length - 1 do
+                        let p = signature.parameters.[i]
+                        let! id = SpirV.id
+                        yield OpFunctionParameter(argTypes.[i], id)
+                        do! SpirV.setId p.name id
+
+
+                    do! assembleStatement body
+
+                    yield OpFunctionEnd
+                    do! SpirV.setId signature id
+
+                    ()
+
+                | CValueDef.CEntryDef e ->
+                    do! assembleEntryDef cond e
+        }
+
+    let assembleModuleS (m : CModule) =
+        spirv {
+            let usage = CModule.usageInfo m
+
+            // assemble all used types
+            for t in usage.usedTypes do
+                let! _ = assembleType t
+                ()
+
+            for v in m.values do
+                do! assembleValueDef None v
+
+
+        }
+
+    let assembleModule (m : CModule) : SpirV.Module =
+        let mutable state = 
+            {
+                currentId           = 0u
+                valueIds            = HashMap.empty
+                uniformIds          = Map.empty
+                fieldIds            = HashMap.empty
+                reversedInstuctions = []
+            }
+        assembleModuleS(m).Run(&state)
+        {
+            magic = 0x07230203u
+            version = 0x00010000u
+            generatorMagic = (0xFADEu <<< 16) ||| 1u
+            bound = state.currentId
+            reserved = 0u
+            instructions = state.reversedInstuctions |> List.rev
+        }
+
+
