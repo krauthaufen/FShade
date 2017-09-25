23b0edca54ef299f2cdf11b8d477b25cbc892411
krauthaufen <krauthaufen@awx.at>
2017-09-25 17:00:56 +0200

    sync

--- build.fsx
+++ build.fsx
@@ -12,15 +12,31 @@ do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 DefaultSetup.install ["src/FShade.sln"]
 
 Target "Merge" (fun () ->
+    let tempDir = @"bin\Release\MergeTemp"
     
-    let compiled = @"bin\Release\FShade.Imperative.Compiled.dll"
-    let result = @"bin\Release\FShade.Imperative.dll"
+    let input = @"bin\Release\FShade.Imperative.dll"
+    let temp = Path.Combine(tempDir, Path.GetFileName input)
+    let inputPdb = Path.ChangeExtension(input, ".pdb")
+    let tempPdb = Path.ChangeExtension(temp, ".pdb")
     
     let pickler = @"bin\Release\FsPickler.dll"
 
-    compiled |> ILMerge (fun p -> 
-        { p with Internalize = InternalizeTypes.Internalize; Libraries = [pickler] }
-    ) result
+    if not (Directory.Exists tempDir) then 
+        Directory.CreateDirectory tempDir |> ignore
+
+
+    input |> ILMerge (fun p ->  
+        { p with TargetKind = TargetKind.Library; Internalize = InternalizeTypes.NoInternalize; Libraries = [pickler]; SearchDirectories = [".."] }
+    ) temp
+
+    File.Delete input
+    File.Move(temp, input)
+    if File.Exists inputPdb then
+        File.Delete inputPdb
+        File.Move(tempPdb, inputPdb)
+
+    Directory.Delete(tempDir, true)
+
 )
 
 "Merge" ==> "CreatePackage"
