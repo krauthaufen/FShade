7dc91ec908c693fb7e0e2bfe460f71ae94c08c85
krauthaufen <krauthaufen@awx.at>
2018-05-29 10:22:54 +0200

    [Tests] moved samplers to types (workaround for module initializer problem in xunit)

--- src/Tests/FShade.Core.Tests/Hashes.fs
+++ src/Tests/FShade.Core.Tests/Hashes.fs
@@ -13,90 +13,96 @@ module Hashes =
             [<Semantic("TexCoord")>] tc : V2d
         }
 
-    module Shader1 =
+    type Shader1 private () =
 
-        let sam =
+        static member Sampler =
             sampler2d {
                 texture uniform?texture
                 filter Filter.MinMagMipLinear
                 addressU WrapMode.Wrap
                 addressV WrapMode.Wrap
             }
-
-        let shader (v : Vertex) =
+            
+        static member shader (v : Vertex) =
             fragment {
-                return sam.Sample(v.tc)
+                return Shader1.Sampler.Sample(v.tc)
             }
+            
+    type Shader2 private () =
 
-    module Shader2 =
-    
-        let sam =
+        static member Sampler =
             sampler2d {
                 texture uniform?texture
                 filter Filter.MinMagPoint
                 addressU WrapMode.Wrap
                 addressV WrapMode.Wrap
             }
-
-        let shader (v : Vertex) =
+            
+        static member shader (v : Vertex) =
             fragment {
-                return sam.Sample(v.tc)
+                return Shader2.Sampler.Sample(v.tc)
             }
+            
 
-    module Shader3 =
+    type Shader3 private () =
 
-        let sam =
+        static member Sampler =
             sampler2d {
                 texture uniform?texture
                 filter Filter.MinMagMipLinear
                 addressU WrapMode.Wrap
                 addressV WrapMode.Wrap
             }
-
-        let shader (v : Vertex) =
+            
+        static member shader (v : Vertex) =
             fragment {
-                return sam.Sample(v.tc)
+                return Shader3.Sampler.Sample(v.tc)
             }
             
-    module Shader4 =
+            
+    type Shader4 private() =
 
         [<GLSLIntrinsic("someFun({0})")>]
-        let sepp (a : V2d) : V4d =
+        static let sepp (a : V2d) : V4d =
             onlyInShaderCode "sepp"
 
-        let shader (v : Vertex) =
+        static member shader (v : Vertex) =
             fragment {
                 return sepp v.tc
             }
             
-    module Shader5 =
+    type Shader5 private() =
 
         [<GLSLIntrinsic("someFun({0})")>]
-        let heinz (a : V2d) : V4d =
+        static let heinz (a : V2d) : V4d =
             onlyInShaderCode "sepp"
 
-        let shader (v : Vertex) =
+        static member shader (v : Vertex) =
             fragment {
                 return heinz v.tc
             }
 
-    [<Fact(Skip = "static initializers broken in xunit")>]
-    let ``[Hashing] static init running``() =
-        Shader1.sam |> should not' (be Null)
-
-    [<Fact(Skip = "static initializers broken in xunit")>]
+    [<Fact>]
     let ``[Hashing] includes SamplerState``() =
 
-        let e1 = Effect.ofFunction Shader1.shader
-        let e2 = Effect.ofFunction Shader2.shader
+        let e1 = Effect.ofFunction (Shader1.shader)
+        let e2 = Effect.ofFunction (Shader2.shader)
 
         e1.Id |> should not' (equal e2.Id)
+        
+    [<Fact>]
+    let ``[Hashing] SamplerState hash works``() =
+
+        let e1 = Effect.ofFunction (Shader1.shader)
+        let e2 = Effect.ofFunction (Shader1.shader)
+
+        e1.Id |> should equal e2.Id
 
     [<Fact>]
     let ``[Hashing] SamplerState hash is structural``() =
 
-        let e1 = Effect.ofFunction Shader1.shader
-        let e2 = Effect.ofFunction Shader3.shader
+        let e1 = Effect.ofFunction (Shader1.shader)
+        let e2 = Effect.ofFunction (Shader3.shader)
 
         e1.Id |> should equal e2.Id
 
