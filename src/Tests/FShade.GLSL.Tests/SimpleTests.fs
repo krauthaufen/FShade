1717aa6cfd3c55194cea9a188642938e14bb2af6
Lukas Prost <prost.lukas@hotmail.com>
2018-06-15 19:00:47 +0200

    added two test cases to test filling an array with helper functions

--- src/Tests/FShade.GLSL.Tests/SimpleTests.fs
+++ src/Tests/FShade.GLSL.Tests/SimpleTests.fs
@@ -95,3 +95,79 @@ let ``PointSize shader``() =
 
 
 
+
+[<ReflectedDefinition>]
+let fillArray (array : Arr<N<10>, V4d>) denom = 
+    for i in 0 .. 9 do
+        array.[i] <- V4d(float i / denom)
+    
+
+[<Fact>]
+let ``Fill Array with Function``() =
+    Setup.Run()
+
+    let frag (v : Vertex) =
+        fragment {
+            let array = Arr<N<10>, V4d>()
+
+            10.0 |> fillArray array
+
+            // using this instead works
+            // let array = fillArray array 10
+
+            return array.[uniform?color]
+        }
+        
+    let expectedFillArrayGLSL =
+        sprintf "
+void .*_fillArray_.*\(vec4 array\[10], float denom\)
+{
+    for\(int i = 0; \(i < 10\); i\+\+\)
+    {
+        array\[i] = vec4\(\(float\(i\) \/ denom\)\);
+    }
+}"
+
+    let expectedMainGLSL =
+        sprintf "
+    vec4 array\[10\];
+    .*_fillArray_.*\(array, 10\.0\);
+    ColorsOut = array1\[color\]"
+
+                
+    GLSL.shouldCompileAndContainRegex [ Effect.ofFunction frag ] [ expectedFillArrayGLSL; expectedMainGLSL ]
+
+[<ReflectedDefinition>][<Inline>]
+let fillArrayInline (array : Arr<N<10>, V4d>) denom = 
+    for i in 0 .. 9 do
+        array.[i] <- V4d(float i / denom)
+    
+
+[<Fact>]
+let ``Fill Array with Inline Function``() =
+    Setup.Run()
+
+    let frag (v : Vertex) =
+        fragment {
+            let array = Arr<N<10>, V4d>()
+
+            10.0 |> fillArrayInline array 
+
+            // using this instead works
+            //fillArrayInline array 10.0
+
+            return array.[uniform?color]
+        }
+        
+    let expectedGLSL =
+        sprintf "
+    vec4 array\[10];
+    for\(int i = 0; \(i < 10\); i\+\+\)
+    {
+        array\[i\] = vec4\(\(float\(i\) \/ 10\.0\)\);
+    }
+    ColorsOut = array\[color\];"
+                
+    GLSL.shouldCompileAndContainRegex [ Effect.ofFunction frag ] [ expectedGLSL ]
+
+
