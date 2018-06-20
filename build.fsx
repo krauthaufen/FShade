6698f4187ddce4772eba4042d995565ddc9bd6c7
krauthaufen <krauthaufen@awx.at>
2018-06-20 08:53:53 +0200

    fixed sourcelink

--- build.fsx
+++ build.fsx
@@ -11,6 +11,17 @@ do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/FShade.sln"]
 
+Target "SourceLink.Test" (fun _ ->
+    !! "bin/*.nupkg" 
+    |> Seq.iter (fun nupkg ->
+        DotNetCli.RunCommand
+            (fun p -> { p with WorkingDir = __SOURCE_DIRECTORY__ @@ "src" @@ "Tests" @@ "FShade.Core.Tests" } )
+            (sprintf "sourcelink test %s" nupkg)
+    )
+)
+
+"CreatePackage" ==> "SourceLink.Test"
+"SourceLink.Test" ==> "Push"
 
 #if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
