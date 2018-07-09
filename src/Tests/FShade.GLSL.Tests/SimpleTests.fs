54a00c90cfbfe667c32f7d924cfc6e08ef54f560
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-07-09 11:01:51 +0200

    added helper function test

--- src/Tests/FShade.GLSL.Tests/SimpleTests.fs
+++ src/Tests/FShade.GLSL.Tests/SimpleTests.fs
@@ -235,6 +235,55 @@ let ``Nested Double Inline``() =
         }
         
     GLSL.shouldCompile [ Effect.ofFunction frag ] 
+
+[<ReflectedDefinition;AutoOpen>]
+module Helper =
+    let hsv2rgb (h : float) (s : float) (v : float) =
+        let s = clamp 0.0 1.0 s
+        let v = clamp 0.0 1.0 v
+
+        let h = h % 1.0
+        let h = if h < 0.0 then h + 1.0 else h
+        let hi = floor ( h * 6.0 ) |> int
+        let f = h * 6.0 - float hi
+        let p = v * (1.0 - s)
+        let q = v * (1.0 - s * f)
+        let t = v * (1.0 - s * ( 1.0 - f ))
+        match hi with
+            | 1 -> V3d(q,v,p)
+            | 2 -> V3d(p,v,t)
+            | 3 -> V3d(p,q,v)
+            | 4 -> V3d(t,p,v)
+            | 5 -> V3d(v,p,q)
+            | _ -> V3d(v,t,p)
+    
+    type UniformScope with 
+        member x.RegionBuffer : array<int> = uniform?StorageBuffer?RegionBuffer
+       
+
+[<Fact>]
+let ``Bad Helpers``() =
+    Setup.Run()
+
+    let c () =
+        compute {
+            let id = getGlobalId()
+            let urdar =
+                match id.X with
+                    | 0 -> 
+                        let a = uniform.RegionBuffer.[id.X]
+                        hsv2rgb (float a) 1.0 1.0
+                    | _ -> 
+                        let b = uniform.RegionBuffer.[0]
+                        V3d.III
+            uniform.RegionBuffer.[id.X] <- int urdar.X
+        }
+    
+    let a = ComputeShader.ofFunction V3i.III c |> ComputeShader.toModule
+    let s = ModuleCompiler.compileGLSLVulkan a
+    printfn "%A" s.code
+    //GLSL.compile [ Effect.ofFunction c ]
+    //GLSL.shouldCompileAndContainRegex [ Effect.ofFunction frag ] [ expectedFillArrayGLSL; expectedMainGLSL ]
     
 [<ReflectedDefinition>]
 let util (a : ref<float>) =
